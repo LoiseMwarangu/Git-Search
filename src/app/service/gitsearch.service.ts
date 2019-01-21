f4abbbfd70b17d7e25471ea89c0651c810f8dce6
Loise Mwarangu <loisemwarangu@gmail.com>
2019-01-21 22:39:26 +0300

    added client secret and client id to prevent restrictions to access

--- src/app/service/gitsearch.service.ts
+++ src/app/service/gitsearch.service.ts
@@ -1,16 +1,20 @@
 import {Injectable} from '@angular/core' ;
-import {HTTP_PROVIDERS} from '@angular/http';
-import 'rxjs/add/operator/map' ;
+import {http,Headers} from '@angular/http';
+import {map} from operator/axjs ;
 
 @Injectable(
     {providedIn:'root'}
 )
 export class GitsearchService{
-    private username='LoiseMwarangu'
-    constructor(private _http:HttpClient){
-        console.log('Github service started')
-    }
+    private clientid='63803c21ae36ca102d8f';
+    private secret='035207e594845697a248822218ac167e7735bb0b'
+    private username='LoiseMwarangu';
+    constructor(private http:HttpClient){
+        console.log('Gitsearch service started')
+    };
     getUser(){
-        return this._http.get('https://api.github.com/users/)'+this.username)
-        .map(res: Response) =>res.json();
+        return this._http.get('https://api.github.com/users/'+this.username)
+        pipe(
+
+        )
     };    
