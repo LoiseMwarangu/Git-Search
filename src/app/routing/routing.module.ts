05550dca97958cc90179d15c9125b1e888ef13ca
Loise Mwarangu <loisemwarangu@gmail.com>
2019-01-23 02:35:33 +0300

    added routes array then moved them to new routing module

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes} from '@angular/router';
+import { GitProfileComponent } from '../git-profile/git-profile.component';
+import { IntroComponent } from '../intro/intro.component';
+import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
+
+const routes:Routes = [
+  {path:"git-profile",component:GitProfileComponent},
+  {path:"intro",component:IntroComponent},
+  {path:"",redirectTo:"/intro",pathMatch:"full"},
+  {path:"**",component:PageNotFoundComponent}
+]
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule],
+  declarations: []
 })
 export class RoutingModule { }
