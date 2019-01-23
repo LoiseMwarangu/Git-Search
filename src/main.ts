805c5a46c8c8626523ff9c1717f57ed33c2de0d6
Loise Mwarangu <loisemwarangu@gmail.com>
2019-01-23 11:16:11 +0300

    moved environment file back

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-// import { enableProdMode } from '@angular/core';
-// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-// import { AppModule } from './app/app.module';
-// // import {environment} from '../environments'
-// // if (environment.production) {
-//   enableProdMode();
-// }
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+// import {environment} from '../environments'
+// if (environment.production) {
+  enableProdMode();
 
-// platformBrowserDynamic().bootstrapModule(AppModule)
-//   .catch(err => console.error(err));
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
